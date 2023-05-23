01a72100c28481b38a3852f18d0aa5e639d978f4
hundredvisionsguy <winikkc@hsd.k12.or.us>
2023-05-22 17:15:55 -0700

    Documenting potential shuffle algorithms

--- python_cardgame_framework/Hand.py
+++ python_cardgame_framework/Hand.py
@@ -24,6 +24,33 @@ class Hand:
     def clear(self):
         self.cards = []
     
+    def shuffle(self):
+        """shuffles all cards in the hand
+        
+        Algorithm 1:
+        GET deck (list or array) 
+        INIT shuffled_deck (list or array) 
+        FOR length(deck): 
+            INIT chosen_card as range(deck) 
+            APPEND shuffled_deck with chosen_card 
+            REMOVE chosen_card from deck 
+        Return shuffled_deck
+
+        Algorithm 2:
+        REPEAT half length of list
+            SET position to random number between 0 and length of list
+            REMOVE card from deck at position
+            APPEND card to end of list.
+        ENDREPEAT
+
+        Algorithm 3:
+        SET shuffled to empty list
+        WHILE deck is not empty
+            REMOVE (pop) card from random position in deck
+            APPEND card to shuffled
+        SET deck to shuffled
+        """
+
     def __str__(self):
         """print the cards, but only if the hand is not empty"""
         if self.cards:
